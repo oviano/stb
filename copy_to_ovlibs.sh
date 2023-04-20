d7653fa4cbbc15afdc827a90c2b9841d4deacd67
Oliver Collyer <ovcollyer@mac.com>
2023-04-20 17:31:24 +0100

    Added copy_to_ovlibs.sh

---
+++ copy_to_ovlibs.sh
@@ -0,0 +1,6 @@
+rm -rf ../../ovlibs/ovgfx/thirdparty/stb
+mkdir ../../ovlibs/ovgfx/thirdparty/stb
+cp stb_image_resize.h ../../ovlibs/ovgfx/thirdparty/stb/
+cp stb_image_write.h ../../ovlibs/ovgfx/thirdparty/stb/
+cp stb_image.h ../../ovlibs/ovgfx/thirdparty/stb/
+cp stb_rect_pack.h ../../ovlibs/ovgfx/thirdparty/stb/
